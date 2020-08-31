1182468d829c881554b7b2346e91d3da893d8024
yifangalter <1727047896@qq.com>
2020-08-31 15:50:12 +0800

    登录完成

---
+++ js/login.js
@@ -0,0 +1,29 @@
+$(function(){
+    $(".submit").click(async function(e){
+        let account = $(".userName").val().trim();
+        let password = $(".userPass").val().trim();
+        if(account===""||password===""){
+            alert("账号和密码不能为空~");
+            return;
+        }
+        password=md5(password);
+        // console.log(account,password);
+
+        // axios.post("/user/login",{
+        //     account,
+        //     password
+        // }).then(res=>{
+        //     console.log(res);
+        // }).catch(err=>{
+        //     console.log(err);
+        // })
+        let res = await axios.post("/user/login",{ account, password})
+
+        if(parseInt(res.code) === 0){
+            alert("登录成功")
+            window.location.href="index.html"
+            return ;
+        }
+        alert("用户名和密码出错了")
+    })
+})
